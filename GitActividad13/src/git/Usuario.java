19d6dad896d71f0ee92e5871e79bf9749d33ad43
Yinary_Hdz <hernandezyinary@gmail.com>
2024-02-19 12:10:27 +0100

    Creado el metodo: Mostrar las valoraciones

--- GitActividad13/src/git/Usuario.java
+++ GitActividad13/src/git/Usuario.java
@@ -4,4 +4,15 @@ public class Usuario {
 	String id;
 	String nombre;
 	double []valoraciones;
+	
+	
+	//METODOS
+	
+	//Mostrar las valoraciones 
+	public void motrarValoraciones() {
+		for(int i = 0;i < this.valoraciones.length ;i++) {
+			System.out.println("Lista de valoraciones");
+			System.out.println("Valoracion  " + i + " : " +  this.valoraciones[i]);	
+		}
+	}
 }
