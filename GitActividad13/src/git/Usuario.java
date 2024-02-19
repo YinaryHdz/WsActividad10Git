00b16430eb72a5982b4f37701adcb019d9262dfc
elenitaferr <fernandezcanizarese@gmail.com>
2024-02-19 12:17:35 +0100

    add funcion devolverMayorMenor y devolverMayorMenorTernario

--- GitActividad13/src/git/Usuario.java
+++ GitActividad13/src/git/Usuario.java
@@ -4,4 +4,18 @@ public class Usuario {
 	String id;
 	String nombre;
 	double []valoraciones;
+	public boolean devolverMayorOMenorMediaValoracion(double nota) {
+			double media = this.devolverValoracionMedia();
+			
+			boolean flag = false;
+			if (nota>media) {
+				flag= true;
+			}
+			return flag;
+		}
+		
+		public boolean devolverMayorOMenorMediaValoracionTernario(double nota) {
+			return (nota>this.devolverValoracionMedia())?true:false;
+		}
+	
 }
