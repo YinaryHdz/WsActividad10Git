ba39ee3a76ec990faf4c0288010f045f58dba067
alesandrani <alesandrani@outlook.es>
2024-02-19 12:09:23 +0100

    primer commit

--- GitActividad13/src/git/Usuario.java
+++ GitActividad13/src/git/Usuario.java
@@ -1,7 +1,37 @@
 package git;
 
+import java.util.Arrays;
+
 public class Usuario {
 	String id;
 	String nombre;
 	double []valoraciones;
+	
+	// Constructores
+		public Usuario() {
+			super();
+		}
+		public Usuario(String id, String nombre, double[] valoraciones) {
+			super();
+			this.id = id;
+			this.nombre = nombre;
+			this.valoraciones = valoraciones;
+		}
+		// Metodos
+		
+		@Override
+		public String toString() {
+			return "Usuario [id=" + id + ", nombre=" + nombre + ", valoraciones=" + Arrays.toString(valoraciones) + "]";
+		}
+		
+		public double devolverValoracionMedia() {
+			double media = 0;
+			double suma = 0;
+			for(int i = 0; i<this.valoraciones.length;i++) {
+				suma += this.valoraciones[i];
+			}
+			media = suma/this.valoraciones.length;
+			return media;
+			
+		}
 }
