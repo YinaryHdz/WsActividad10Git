c4bb330575ee477d9a29f8612054cd5d9f546f4b
Avril2504 <avrilpino@gmail.com>
2024-02-19 12:08:10 +0100

    GitActividad13

--- GitActividad13/src/git/Usuario.java
+++ GitActividad13/src/git/Usuario.java
@@ -4,4 +4,15 @@ public class Usuario {
 	String id;
 	String nombre;
 	double []valoraciones;
+
+public int devolverPuntuacionesMasAltas(double nota) {
+	int notas_mayores = 0;
+	for (double d : valoraciones) {
+		if (d>nota) {
+			notas_mayores++;
+		}
+
+	}
+	return notas_mayores;
+	}
 }
